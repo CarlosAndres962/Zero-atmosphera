cd45fc22e28caec54d59563b59726de1ca6f5813
MendozaAl144 <mendozaalvaro940@gmail.com>
2025-02-19 19:18:49 +0100

    Add files via upload

---
+++ Aerocars.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.zeroatmosphera;
+
+/**
+ *
+ * @author mendo
+ */
+public class Aerocars extends Vehiculos{
+    private final double precio=12;
+    private double distancia;
+    private String matricula;
+    private String tipo;
+    private ResponsableVehiculo nombre;
+
+    public Aerocars(double distancia, String matricula, String tipo, int numper, double velocidad, double largo, double ancho) {
+        super(numper, velocidad, largo, ancho);
+        this.distancia = distancia;
+        this.matricula = matricula;
+        this.tipo = tipo;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia() {
+        this.distancia = distancia;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula() {
+        this.matricula = matricula;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public double calculaCarburante(){
+        double raiz= Math.sqrt(distancia);
+        return raiz*precio;
+    }
+    
+}
---
+++ Vehiculos.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.zeroatmosphera;
+
+/**
+ *
+ * @author mendo
+ */
+public abstract class Vehiculos {
+    protected int numper;
+    protected double velocidad;
+    protected double largo;
+    protected double ancho;
+    
+    public Vehiculos(int numper,double velocidad,double largo,double ancho){
+        this.numper=numper;
+        this.velocidad=velocidad;
+        this.largo=largo;
+        this.ancho=ancho;
+    }
+
+    public int getNumper() {
+        return numper;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setNumper(int numper) {
+        this.numper = numper;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+    
+}
