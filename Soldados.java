6d1d2cec39a87620f5c7567bba7032126492d933
IvanDiazSilva <ivandiazsilva70@gmail.com>
2025-02-20 16:36:13 +0100

    ivan

--- Soldados.java
+++ Soldados.java
@@ -8,6 +8,56 @@ package zeroatmosf;
  *
  * @author ivand
  */
-public class Soldados {
+public class Soldados extends Tripulacion {
+
+    public int graduacion;
+
+    public Soldados() {
+    }
+
+    public Soldados(int graduacion, String nombre, char sexo, String codigo) {
+        super(nombre, sexo, codigo);
+        this.graduacion = graduacion;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public char getSexo() {
+        return sexo;
+    }
+
+    @Override
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
     
+
+    public int getGraduacion() {
+        return graduacion;
+    }
+
+    public void setGraduacion(int graduacion) {
+        this.graduacion = graduacion;
+    }
+    
+
 }
