e90e7fc32a8a6d255231c883c3c07c5f0cc440c6
ERM1999 <ejb.1999@hotmail.com>
2025-02-21 18:01:56 +0100

    Update Mascotas.java

--- Mascotas.java
+++ Mascotas.java
@@ -11,44 +11,11 @@ package zeroatmosphere;
 public abstract class Mascotas extends Tripulacion {
     
     //Atributos
-    private static int ncod=0;
-    public String nombre;
-    public String propietario;
-    public String codigo;
-    public int edad;
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public Mascotas(String nombre, String codigo) {
+        super(nombre, codigo);
     }
-
-    public void setPropietario(String propietario) {
-        this.propietario = propietario;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getPropietario() {
-        return propietario;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
+     
     //Constructor
     
     public abstract String Comunican();
@@ -56,9 +23,6 @@ public abstract class Mascotas extends Tripulacion {
     public abstract String Duermen();
     
     public abstract String Comen();
-    
-    public String generaCodigo(){
-    return "mas-"+String.format("%04d"+ncod++);
-    }
+
 
 }
