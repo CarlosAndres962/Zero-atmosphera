7fc0b8bae0d70112f38eed12eb8822439748d0e8
MendozaAl144 <mendozaalvaro940@gmail.com>
2025-03-03 17:00:00 +0100

    Add files via upload

--- Validador.java
+++ Validador.java
@@ -11,28 +11,24 @@ package zeroatmospher;
 public class Validador {
     public Validador(){}
     
-    public void validarnumero(String numero) throws Excepciones {
-        // Verifica si la entrada es un número decimal
-        if (numero.contains(".")) {
-            throw new Excepciones("El número no puede ser un decimal.");
-        }
+    public void validarnumero(int numero) throws Excepciones {
 
         // Verifica si la entrada es un número entero válido
         try {
-            int num = Integer.parseInt(numero);
-            if (num < 0) {
-                throw new Excepciones("Error: El número de alienígenas no puede ser negativo.");
+            
+            if (numero < 0) {
+                throw new Excepciones("Error: El número no puede ser negativo.");
             }
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {// Verifica si la entrada es un número decimal
             throw new Excepciones("Error: La entrada no es un número entero válido.");
         }
     }
     public void validaChar(String confirmacion) throws Excepciones{
-        if (confirmacion.length()>1 || confirmacion.length()==0) {
+        if (confirmacion.length()!=1) {
             throw new Excepciones("Error: La entrada debe contener un digito.");
         }
         char caracter=confirmacion.charAt(0);
-        if (caracter!='s' || caracter!='n') {
+        if (caracter!='s' && caracter!='n') {
             throw new Excepciones("Error: La entrada no es válido solo 's' o 'n'.");
         }
     } 
