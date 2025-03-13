86698cc8d9f468c59b68da78af4a95981e5e785b
IvanDiazSilva <ivandiazsilva70@gmail.com>
2025-03-13 17:05:11 +0100

    BinarioMartillo

--- fichero3.java
+++ fichero3.java
@@ -1,7 +1,71 @@
 
 package beta;
 
+import java.io.Serializable;
 
-public class fichero3 {
-    
+public class Martillo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String nombre; //NOMBRE DE LA MAQUINA
+    private int consumo; //VARIABLE DE 200 A 300KW
+    private String sujeccion; // MANUAL/CORREA
+    private double proteccion; //VARIABLE DE 0 A 1 
+
+    public Martillo(String nombre, int consumo, String sujeccion, double proteccion) {
+        this.nombre = nombre;
+        setConsumo(consumo);  //VALIDAMOS EL RANGO(200-300)
+        setSujeccion(sujeccion);//VALIDAMOS QUE SEA "MANUAL O "CORREA"
+        setProteccion(proteccion);//VALIDAMOS EL RANGO(0-1)
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(int consumo) {
+        if (consumo >= 200 && consumo <= 300) {
+            this.consumo = consumo;
+
+        } else {
+            throw new IllegalArgumentException("Consumo debe estar entre 200 y 300 kw");
+        }
+    }
+
+    public String getSujeccion() {
+        return sujeccion;
+    }
+
+    public void setSujeccion(String sujeccion) {
+        if (sujeccion.equals("manual") || sujeccion.equals("correa")) {
+            this.sujeccion = sujeccion;
+        } else {
+            throw new IllegalArgumentException("Sujeccion debe ser 'manual' o 'correa'");
+
+        }
+    }
+
+    public double getProteccion() {
+        return proteccion;
+    }
+
+    public void setProteccion(double proteccion) {
+        if (proteccion >= 0 && proteccion <= 1) {
+            this.proteccion = proteccion;
+        } else {
+            throw new IllegalArgumentException("Proteccion debe estar entre 0 y 1. ");
+        }
+    }
+
+    //METODO PARA MOSTRAR INFORMACION DEL MARTILLO
+    public void mostrarInfo() {
+        System.out.println("Martillo: " + nombre);
+        System.out.println("Consumo: " + consumo);
+        System.out.println("Sujeccion: " + sujeccion);
+        System.out.println("Proteccion: " + proteccion);
+    }
+
 }
