147f8473938e6b00f9c03bab27d592475ac0104b
CarlosAndres962 <azcarateruiz@gmail.com>
2025-03-13 17:13:47 +0100

    Update Tripulacion.java

--- Tripulacion.java
+++ Tripulacion.java
@@ -1,28 +1,19 @@
 
-package zeroatmosf;
+package beta;
+import java.util.Scanner;
 
 
-public abstract class Tripulacion {
-    //Atributos
+public abstract class tripulacion implements Armas {
+      //Atributos
     protected String nombre;
     protected char sexo;
     
     //Construtores
-    public Tripulacion() {
+    public tripulacion() {
+   
     }
 
-    public Tripulacion(String nombre,char sexo) {
-        this.nombre = nombre;
-        this.sexo=sexo;
-    }
-    
-
-    public Tripulacion(String nombre, String codigo) {
-        this.nombre = nombre;
-    }
-    
-
-    public Tripulacion(String nombre, char sexo, String codigo) {
+    public tripulacion(String nombre, char sexo) {
         this.nombre = nombre;
         this.sexo = sexo;
     }
@@ -41,9 +32,26 @@ public abstract class Tripulacion {
 
     public void setSexo(char sexo) {
         this.sexo = sexo;
+    } 
+    public abstract String getCodigo();
+
+    @Override
+    public String armamento() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escoge qué arma quieres que usen: ");
+        int n = sc.nextInt();
+        if(n==1){
+            return"rayo laser";
+        }
+        else if(n==2){
+            return"Espada luminica";
+        }
+        else if(n==3){
+            return "Pistola espacial";
+        }
+        else
+            return "Numero incorrecto";
     }
     
-    public abstract String getCodigo();
-    
-    
 }
+
