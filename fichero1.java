3d15c7092ba92dc74f87f9e8f6ed398f52d7c7fd
MendozaAl144 <mendozaalvaro940@gmail.com>
2025-03-19 18:27:38 +0100

    Update fichero1.java

--- fichero1.java
+++ fichero1.java
@@ -14,9 +14,12 @@ public class CiberExcavadora implements Serializable{
 
     public CiberExcavadora(String nombre, int consumo, String traccion, double proteccion) {
         this.nombre = nombre;
-        setConsumo(consumo);//VALIDAMOS EL RANGO DEL CONSUMO.
-        setTraccion(traccion); // VALIDAMOS ENTRE RUEDAS U ORUGAS.
-        setProteccion(proteccion); //VALIDAMOS EL RANGO DE PROTECCION ENTRE 0 Y 1.
+        try{
+            setConsumo(consumo);//VALIDAMOS EL RANGO DEL CONSUMO.
+            setTraccion(traccion); // VALIDAMOS ENTRE RUEDAS U ORUGAS.
+            setProteccion(proteccion); //VALIDAMOS EL RANGO DE PROTECCION ENTRE 0 Y 1.
+        }catch(Excepciones e)
+        {System.err.println("Error: "+e);}
     }
 
     //METODOS GETTERS Y SETTERS CON VALIDACIONES
@@ -28,11 +31,11 @@ public class CiberExcavadora implements Serializable{
         return consumo;
     }
 
-    public void setConsumo(int consumo) {
+    public void setConsumo(int consumo) throws Excepciones{
         if (consumo >= 1 && consumo <= 5) {
             this.consumo = consumo;
         } else {
-            throw new IllegalArgumentException("Consumo debe estar entre 1 y 5");
+            throw new Excepciones("Consumo debe estar entre 1 y 5");
         }
     }
 
@@ -40,12 +43,11 @@ public class CiberExcavadora implements Serializable{
         return traccion;
     }
 
-    public void setTraccion(String traccion) {
-        if (traccion.equals("ruedas") || traccion.equals("oruga")) {
-            this.traccion = traccion;
-
+    public void setConsumo(int consumo) throws Excepciones{
+        if (consumo >= 1 && consumo <= 5) {
+            this.consumo = consumo;
         } else {
-            throw new IllegalArgumentException("Traccion debe ser 'ruedas' u 'oruga'.");
+            throw new Excepciones("Consumo debe estar entre 1 y 5");
         }
     }
 
@@ -53,13 +55,14 @@ public class CiberExcavadora implements Serializable{
         return proteccion;
     }
 //METODO PARA VALIDAR PROTECCION
-    public void setProteccion(double proteccion) {
+    public void setProteccion(double proteccion) throws Excepciones{
         if (proteccion >= 0 && proteccion <= 1) {
             this.proteccion = proteccion;
 
         } else {
-            throw new IllegalArgumentException("Proteccion debe estar entre 0 y 1 ");
+            throw new Excepciones("Proteccion debe estar entre 0 y 1 ");
         }
+    }
 
    @Override
     public String toString() {
