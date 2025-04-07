51f36391593a9b85fc841445dacbcee20537aab7
ERM1999 <ejb.1999@hotmail.com>
2025-04-08 00:19:33 +0200

    Update ZeroAtmosphere.java

--- ZeroAtmosphere.java
+++ ZeroAtmosphere.java
@@ -1,5 +1,7 @@
 package zeroatmosphere;
 
+package zeroatmosphere;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,9 +9,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
-import java.util.Random;
 import java.util.Scanner;
 
+
+
+//1 ganando se cierra y perdiendo
 /**
  *
  * @author fani
@@ -27,6 +31,11 @@ public class ZeroAtmosphere {
     static ArrayList<Soldados> solArray = new ArrayList<>();
     static ArrayList<Mineros> minArray = new ArrayList<>();
     
+    static ArrayList<Mascotas> mascotas = new ArrayList<>();
+    static ArrayList<Persona> personas = new ArrayList<>();
+    static ArrayList<Aerobikes> aerobikes = new ArrayList<>();
+    static ArrayList<Aerocars> aerocars = new ArrayList<>();
+  
     static Validador val=new Validador();
     
     
@@ -53,6 +62,7 @@ public class ZeroAtmosphere {
                     mostrarEstadoNave();
                     break;
                 case "2":
+                    opcion2();
                     break;
                 case "3":
                     if (operacionEnCurso) {
@@ -151,16 +161,15 @@ public class ZeroAtmosphere {
         cantidadSoldados = alienigenas * 2; // El número de soldados es el doble de alienígenas
         cantidadMineros = alienigenas * 2;  // El número de mineros es el doble de alienígenas
       
-        crearSoldados(); 
-        crearMineros();  
+        crearSoldados(cantidadSoldados); 
+        crearMineros(cantidadMineros);  
         crearAlienigenas();
     
         System.out.println("\n--- Información genérica de la operación ---");
         nombresSoldados(solArray);
         nombresMineros(minArray);
         System.out.println("alienígenas esperados: "+ alienigenas);
-        System.out.println("LAURA, A LA ESPERA DE CREAR TRIPULACION");
-        System.out.println("AEROCARS, A LA ESPERA DE CREAR TRIPULACION");
+
     
         System.out.println("\n--- Códigos  ---");
         codigosAlienigenas();
@@ -231,7 +240,7 @@ public class ZeroAtmosphere {
     return sexo;
 }
    
-    public static void crearSoldados(){
+    public static void crearSoldados(int cantidad){
     String[] nombresMasculinos = {"Juan", "Carlos", "Miguel", "Luis", "Pedro", "José", "Antonio"};
     String[] nombresFemeninos = {"María", "Ana", "Laura", "Carmen", "Sofía", "Isabel", "Elena"};
         
@@ -239,15 +248,8 @@ public class ZeroAtmosphere {
     int contadorFemeninos = 0; //actua como índice para acceder a un elemento específico del array nombresFemeninos
     String nombreSoldado;
     
-   
-    int soldadosACrear; // Declaramos la variable primero
-    if (operacionEnCurso) {
-        soldadosACrear = 2;
-    } else {
-        soldadosACrear = 2;
-    }
           
-    for (int i = 0; i < soldadosACrear; i++) { // bucle se ejecuta una vez por cada soldado en el array sol
+    for (int i = 0; i < cantidad; i++) { // bucle se ejecuta una vez por cada soldado en el array sol
         char sexoSoldado = asignarSexoAleatorio();
         if (sexoSoldado == 'H') {       
             nombreSoldado = nombresMasculinos[contadorMasculinos]+ contadorMasculinos;//accede al elemento del array nombresMasculinos en la posición indicada por contador y se asigna a nombreSoldado
@@ -267,26 +269,17 @@ public class ZeroAtmosphere {
         Soldados soldado = new Soldados((int) (Math.random() * 10) + 1, nombreSoldado, sexoSoldado);
         solArray.add(soldado); //Almacenar el objeto en el array
     }
-    cantidadSoldados += soldadosACrear; // Actualizar la cantidad total de soldados
 
  }      
-    public static void crearMineros(){
+    public static void crearMineros(int cantidad){
     String[] nombresMasculinos = {"Alejandro", "Andrés", "Diego", "Fernando", "Gabriel","Héctor", "Javier"};
     String[] nombresFemeninos = {"Adriana", "Beatriz", "Clara", "Diana", "Elena","Gabriela","Inés"};
         
     int contadorMasculinos = 0; 
     int contadorFemeninos = 0; //actua como índice para acceder a un elemento específico del array nombresFemeninos
-    String nombreMinero;
-    
-    
-    int minerosACrear; // Declaramos la variable primero
-    if (operacionEnCurso) {
-        minerosACrear = 2;
-    } else {
-        minerosACrear = 2;
-    }
+    String nombreMinero;    
         
-    for (int i = 0; i < minerosACrear; i++) { // bucle se ejecuta una vez por cada soldado en el array min
+    for (int i = 0; i < cantidad; i++) { // bucle se ejecuta una vez por cada soldado en el array min
         char sexoMinero = asignarSexoAleatorio();
         if (sexoMinero == 'H') {
             nombreMinero = nombresMasculinos[contadorMasculinos]+contadorMasculinos;//accede al elemento del array nombresMasculinos en la posición indicada por contador y se asigna a nombreSoldado
@@ -305,7 +298,6 @@ public class ZeroAtmosphere {
         Mineros minero = new Mineros((int) (Math.random() * (65 - 18 + 1)) + 18, nombreMinero, sexoMinero);
         minArray.add(minero); 
     }        
-    cantidadMineros += minerosACrear; // Actualizar la cantidad total de soldados
  }
     public static void crearAlienigenas() {
     for (int i = 0; i < alienigenas ; i++) {
@@ -485,8 +477,10 @@ public class ZeroAtmosphere {
 
     // Verificar si el jugador ganó
     if (adivinaNumero.juegoGanado) { // Si el jugador ganó el juego.
-        crearSoldados(); // Llama a un método para crear soldados
-        crearMineros();
+       cantidadSoldados += 2; // Incrementa la cantidad total de soldados en 2
+        cantidadMineros += 2;  // Incrementa la cantidad total de mineros en 2
+        crearSoldados(2);      // Crea 2 nuevos soldados y los añade a la lista
+        crearMineros(2);       // Crea 2 nuevos mineros y los añade a la lista
         System.out.println("¡Ganaste! Ahora tienes 2 soldados y 2 mineros adicionales"); // Imprime un mensaje de victoria.
     } else { // Si el jugador perdió el juego.
         System.out.println("¡Perdiste! Inténtalo de nuevo."); // Imprime un mensaje de derrota.
@@ -506,8 +500,10 @@ public class ZeroAtmosphere {
 
     // Verificar si el jugador ganó
     if (tresEnRaya.juegoGanado) { // Si el jugador ganó el juego.
-        crearSoldados();
-        crearMineros();
+        cantidadSoldados += 2; // Incrementa la cantidad total de soldados en 2
+        cantidadMineros += 2;  // Incrementa la cantidad total de mineros en 2
+        crearSoldados(2);      // Crea 2 nuevos soldados y los añade a la lista
+        crearMineros(2);       // Crea 2 nuevos mineros y los añade a la lista
         System.out.println("¡Ganaste! Ahora tienes 2 soldados y 2 mineros adicionales"); // Imprime un mensaje de victoria.
     } else { // Si el jugador perdió el juego.
         System.out.println("¡Perdiste! Inténtalo de nuevo."); // Imprime un mensaje de derrota.
@@ -526,11 +522,329 @@ public class ZeroAtmosphere {
     }
 
     if (seguirSecuencia.juegoGanado) { // Si el jugador ganó el juego.
-        crearSoldados(); // Llama a un método para crear soldados
-        crearMineros();
+        cantidadSoldados += 2; // Incrementa la cantidad total de soldados en 2
+        cantidadMineros += 2;  // Incrementa la cantidad total de mineros en 2
+        crearSoldados(2);      // Crea 2 nuevos soldados y los añade a la lista
+        crearMineros(2);       // Crea 2 nuevos mineros y los añade a la lista
         System.out.println("¡Ganaste! Ahora tienes 2 soldados y 2 mineros adicionales"); // Imprime un mensaje de victoria.
     } else { // Si el jugador perdió el juego.
         System.out.println("¡Perdiste! Inténtalo de nuevo."); // Imprime un mensaje de derrota.
     }
 }
+    
+    public static void agregarGato() {
+        System.out.println("Introduzca el nombre del gato:");
+        String nombre = sc.next();
+
+        System.out.println("Introduzca el sexo del gato (M/F):");
+        char sexo = sc.next().toUpperCase().charAt(0);
+
+        sc.nextLine(); // Limpia el buffer
+        System.out.println("Introduzca la raza del gato:");
+        String raza = sc.nextLine();
+
+        Gatos gato = new Gatos(nombre, sexo, raza);
+        mascotas.add(gato); // Agregar a la lista
+        System.out.println("Gato agregado correctamente.");
+    }
+    public static void agregarPerro() {
+        System.out.println("Introduca el nombre del perro");
+        String nombre = sc.next();
+
+        System.out.println("Introduzca el sexo del perro (M/F):");
+        char sexo = sc.next().toUpperCase().charAt(0);
+
+        System.out.println("Introduzca la raza del perro");
+        String raza = sc.nextLine();
+
+        Perros perro = new Perros(nombre,raza, sexo);
+        mascotas.add(perro); // Agregar a la lista
+        System.out.println("Perro agregado correctamente.");
+
+    }
+    public static void agregarPajaro() {
+        System.out.println("Introduca el nombre del pajaro");
+        String nombre = sc.next();
+
+        System.out.println("Introduzca el sexo del pájaro (M/F): ");
+        char sexo = sc.next().toUpperCase().charAt(0);
+
+        System.out.println("Introduzca la especie del pajaro");
+        String especie = sc.nextLine();
+
+        Pajaros pajaro = new Pajaros(nombre, sexo, especie);
+        mascotas.add(pajaro);//se agrega al array list
+        System.out.println("Pajaro creado correctamente.");
+
+    }
+    public static void agregarAerobikes() {
+        System.out.println("Agregando Aereobike...");
+        System.out.println("Introduzca el color: ");
+        String color = sc.next();
+        System.out.println("Introduzca el modelo");
+        String modelo = sc.next();
+        System.out.println("Introduzca el tipo del aereobike:");
+        String tipo = sc.next();
+        System.out.println("Introduzca el numero de personas ");
+        int numeropersonas = sc.nextInt();
+        System.out.println("Introduzca la velocidad del aerobike: ");
+        int velocidad = sc.nextInt();
+        System.out.println("Introduzca el numero de turbosjets");
+        int turbojets = sc.nextInt();
+
+        Aerobikes ae1 = new Aerobikes(color, modelo, tipo, numeropersonas, velocidad, turbojets);
+        aerobikes.add(ae1);
+        System.out.println("Aerobike creado correctamente.");
+
+    }
+    public static void agregarAerocars() {
+        System.out.println("Agregando Aereocars...");
+        System.out.println("Introduzca el color: ");
+        double distancia = sc.nextDouble();
+        sc.nextLine();// Para que se limpie el buffer antes de leer el nombrre
+        System.out.println("Introduzca el nombre del responsable: ");
+        String nombreResponsable = sc.next();
+        ResponsableVehiculo responsable = new ResponsableVehiculo(nombreResponsable);
+        System.out.println("Introduzca el numero de personas: ");
+        int numeropersonas = sc.nextInt();
+        System.out.println("Introduzca la velocidad del aerobike: ");
+        int velocidad = sc.nextInt();
+        System.out.println("Introduzca el numero de turbosjets");
+        int turbojets = sc.nextInt();
+
+        Aerocars ae1 = new Aerocars(distancia, responsable, numeropersonas, velocidad, turbojets);
+        aerocars.add(ae1);
+        System.out.println("Aerocar creado correctamente.");
+
+    }
+
+    public static void eliminarMascota() {
+        if (mascotas.isEmpty()) {
+            System.out.println("No hay mascotas registradas para eliminar.");
+            return;
+        }
+
+        System.out.println("Lista de Mascotas:");
+        for (int i = 0; i < mascotas.size(); i++) {
+            System.out.println(i + ". " + mascotas.get(i).toString());
+        }
+
+        System.out.print("Seleccione el índice de la mascota a eliminar: ");
+        int index = sc.nextInt();
+        sc.nextLine(); // Limpiar buffer
+
+        if (index >= 0 && index < mascotas.size()) {
+            mascotas.remove(index);
+            System.out.println("Mascota eliminada correctamente.");
+        } else {
+            System.out.println("Índice inválido.");
+        }
+    }
+    public static void eliminarPersona() {
+        if (personas.isEmpty()) {
+            System.out.println("No hay personas registradas para eliminar.");
+            return;
+        }
+
+        System.out.println("Lista de Personas:");
+        for (int i = 0; i < personas.size(); i++) {
+            System.out.println(i + ". " + personas.get(i).toString());
+        }
+
+        System.out.print("Seleccione el índice de la persona a eliminar: ");
+        int index = sc.nextInt();
+        sc.nextLine(); // Limpiar buffer
+
+        if (index >= 0 && index < personas.size()) {
+            personas.remove(index);
+            System.out.println("Persona eliminada correctamente.");
+        } else {
+            System.out.println("Índice inválido.");
+        }
+    }
+    public static void eliminarAerobike() {
+    if (aerobikes.isEmpty()) {
+        System.out.println("No hay Aerobikes registradas para eliminar.");
+        return;
+    }
+
+    System.out.println("Lista de Aerobikes:");
+    for (int i = 0; i < aerobikes.size(); i++) {
+        System.out.println(i + ". " + aerobikes.get(i).toString());
+    }
+
+    System.out.print("Seleccione el índice de la Aerobike a eliminar: ");
+    int index = sc.nextInt();
+    sc.nextLine(); // Limpiar buffer
+
+    if (index >= 0 && index < aerobikes.size()) {
+        aerobikes.remove(index);
+        System.out.println("Aerobike eliminada correctamente.");
+    } else {
+        System.out.println("Índice inválido.");
+    }
 }
+    public static void eliminarAerocar() {
+    if (aerocars.isEmpty()) {
+        System.out.println("No hay Aerocars registrados para eliminar.");
+        return;
+    }
+
+    System.out.println("Lista de Aerocars:");
+    for (int i = 0; i < aerocars.size(); i++) {
+        System.out.println(i + ". " + aerocars.get(i).toString());
+    }
+
+    System.out.print("Seleccione el índice del Aerocar a eliminar: ");
+    int index = sc.nextInt();
+    sc.nextLine(); // Limpiar buffer
+
+    if (index >= 0 && index < aerocars.size()) {
+        aerocars.remove(index);
+        System.out.println("Aerocar eliminado correctamente.");
+    } else {
+        System.out.println("Índice inválido.");
+    }
+}
+    
+    public static void opcion2(){
+        int opcion = 0;
+int choice = 0;
+
+do {
+System.out.println("1. Consulta tripulaciOn completa y vehIculos");
+System.out.println("2. Modifica tripulaciOn o vehIculos ");
+System.out.println("4. Salir");
+System.out.print("Seleccione una opciOn: ");
+opcion = sc.nextInt();
+sc.nextLine();//Para limoiar el buffer
+
+switch (opcion) {
+case 1:
+System.out.println("Consulta Tripulacion");
+break;
+case 2:
+do {
+System.out.println("1. Agregar Tripulacion");
+System.out.println("2. Eliminar Tripulacion");
+System.out.println("3. Agregar Vehiculos");
+System.out.println("4. Eliminar Vehiculos");
+System.out.println("5. Volver al menu principal");
+System.out.print("Seleccione una opcion: ");
+choice = sc.nextInt();
+sc.nextLine();
+
+switch (choice) {
+case 1:
+System.out.println("Opciones para añadir:");
+System.out.println("1. MASCOTAS");
+System.out.println("2. PERSONAS");
+int opcion1 = sc.nextInt();
+
+if (opcion1 == 1) {
+System.out.println("Introduzca que mascota quiere agregar: ");
+System.out.println("1. Gato");
+System.out.println("2. Perro");
+System.out.println("3. Pajaro");
+int mascota = sc.nextInt();
+if (mascota == 1) {
+agregarGato();
+} else if (mascota == 2) {
+agregarPerro();
+} else {
+agregarPajaro();
+}
+
+Gatos ga1 = new Gatos("Canela", 'F', "Bengali");
+Perros pe1 = new Perros("Rocky","Labrador", 'M');
+Pajaros pa1 = new Pajaros("Rito", 'F', "Loro");
+
+mascotas.add(ga1);
+mascotas.add(pe1);
+mascotas.add(pa1);
+
+} else if (opcion1 == 2) {
+
+System.out.println("Personas");
+Persona p1 = new Persona("Laura", 29, 'F', 67.5, 1.68);
+Persona p2 = new Persona("Carmen", 40, 'F', 100, 1.59);
+Persona p3 = new Persona("Lopez", 32, 'M', 67.5, 1.71);
+Persona p4 = new Persona("Federico", 26, 'M', 67.5, 1.90);
+personas.add(p1);
+personas.add(p2);
+personas.add(p3);
+personas.add(p4);
+
+break;
+} else {
+System.out.println("Opción inválida.");
+}
+break;
+
+case 2:
+System.out.println("Eliminar Tripulacion");
+System.out.println("1. MASCOTAS");
+System.out.println("2. PERSONAS");
+int opcion3 = sc.nextInt();
+if(opcion3 == 1) {
+eliminarMascota();
+} else if (opcion3 == 2) {
+eliminarPersona();
+} else {
+System.out.println("Opción inválida.");
+}
+break;
+
+case 3:
+System.out.println("Opciones para añadir vehículos:");
+System.out.println("1. AEREOBIKES");
+System.out.println("2. AEREOCARS");
+int opcionVehiculo = sc.nextInt();
+sc.nextLine();
+
+if (opcionVehiculo == 1) {
+agregarAerobikes();
+Aerobikes b1 = new Aerobikes("Azul", "first34", "FE-56", 67, 78.8, 98);
+Aerobikes b2 = new Aerobikes("Negro", "SCON67", "WE-99", 56, 23, 34);
+
+} else if (opcionVehiculo == 2) {
+agregarAerocars();
+ResponsableVehiculo rv1 = new ResponsableVehiculo("Ivan");
+Aerocars v1 = new Aerocars(11.3, rv1, 5, 45.2, 4);
+ResponsableVehiculo rv2 = new ResponsableVehiculo("Maria");
+Aerocars v2 = new Aerocars(56.7, rv2, 7, 8.7, 9);
+
+} else {
+System.out.println("Opción inválida.");
+}
+break;
+
+case 4:
+System.out.println("Eliminar Vehiculo");
+System.out.println("1. Aerobikes");
+System.out.println("2. Aerocars");
+int opcion4 = sc.nextInt();
+if(opcion4 == 1) {
+eliminarAerobike();
+} else if (opcion4 == 2) {
+eliminarAerocar();
+} else {
+System.out.println("Opción inválida.");
+}
+
+break;
+
+case 5:
+System.out.println("Volviendo al menú principal...");
+break;
+
+default:
+System.out.println("Opción inválida.");
+}
+} while (choice != 5);
+break;
+
+}
+} while (opcion != 4);
+}
+    }
